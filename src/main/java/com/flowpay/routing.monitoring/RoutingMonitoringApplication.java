1b8be41cadc8578888f926ff50cfce5995547554
Daniel <daniel.razah@gmail.com>
2026-07-01 17:44:36 -0400

    chore: bootstrap project from Spring Initializr
    Spring Boot 4.1.0, Java 21, Gradle.

---
+++ src/main/java/com/flowpay/routing.monitoring/RoutingMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.flowpay.routing.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutingMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutingMonitoringApplication.class, args);
+	}
+
+}
